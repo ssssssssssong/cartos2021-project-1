4c3c066c5c345256b44598a83087b64c22c872be
ssssssssssong <84441235+ssssssssssong@users.noreply.github.com>
2021-05-20 19:21:16 +0900

    Create pratice.c

---
+++ pratice.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define MAX_LEN_LINE    100
+
+int main(void)
+{
+    char command[MAX_LEN_LINE];
+    char *args[] = {command, NULL};
+    int ret, status;
+    pid_t pid, cpid;
+      while (true) {
+        char *s;
+        int len;
+        
+        printf("MyShell $ ");
+        s = fgets(command, MAX_LEN_LINE, stdin);
+        if (s == NULL) {
+            fprintf(stderr, "fgets failed\n");
+            exit(1);
+        }
+        
+        len = strlen(command);
+        printf("%d\n", len);
+      }
+}
